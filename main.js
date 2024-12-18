e91dd8fe00f2fdb01d27a14d6b6cbc4a46306b58
revoirb612 <revoirb612@gmail.com>
2024-12-19 01:49:24 +0900

    initial commit

---
+++ main.js
@@ -0,0 +1,27 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: false
+        }
+    });
+
+    win.loadURL('https://www.pedamint.com');
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
