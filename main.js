498400c6d7f51b0bf42b959c317e6f6e91705e4e
revoirb612 <revoirb612@gmail.com>
2024-12-19 13:10:16 +0900

    Add global shortcut for navigation and set window icon

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, globalShortcut } = require('electron');
 
 function createWindow() {
     const win = new BrowserWindow({
@@ -6,13 +6,28 @@ function createWindow() {
         height: 800,
         webPreferences: {
             nodeIntegration: false
-        }
+        },
+        icon: 'assets/favicon.ico'
     });
 
     win.loadURL('https://www.pedamint.com');
+
+    const webContents = win.webContents;
+
+    // Alt + Left Arrow 키로 뒤로가기
+    globalShortcut.register('Alt+Left', () => {
+        if (webContents.navigationHistory.canGoBack()) {
+            webContents.navigationHistory.goBack();
+        }
+    });
 }
 
 app.on('ready', createWindow);
+
+app.on('will-quit', () => {
+    // 모든 단축키 등록 해제
+    globalShortcut.unregisterAll();
+});
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
